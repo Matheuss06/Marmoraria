d63c7d45c4c8d7c5245620d5fe34b955523af048
Matheuss06 <matheuscs006@gmail.com>
2025-03-19 15:43:47 -0300

    Criação de Header

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Pedra Bela</title>
+</head>
+<body class="fundo">
+
+<!-- Inclusão do Header -->
+<?php include 'header.html' ?>
+
+
+
+
+</body>
+</html>
