21dd88049793222474753e4981cded7ba98e85fa
Pedro <pedrottorres07@gmail.com>
2025-03-20 20:27:22 -0300

    icones redes sociais no footer

--- footer.php
+++ footer.php
@@ -5,11 +5,10 @@
     <div class="footer-section">
       <h3>Guia do Site</h3>
       <ul>
-        <li><a href="#">Página Inicial</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="#">Sobre Nós</a></li>
-        <li><a href="#">Serviços</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contato</a></li>
+        <li><a href="servicos.php">Serviços</a></li>
+        <li><a href="contato.php">Contato</a></li>
       </ul>
     </div>
 
@@ -17,10 +16,10 @@
     <div class="footer-section">
       <h3>Redes Sociais</h3>
       <ul class="social-icons">
-        <li><a href="#"></a></li>
-        <li><a href="#"></a></li>
-        <li><a href="#"></a></li>
-        <li><a href="#"></a></li>
+        <li><a href="#"></a><img src="imgs/facebook.png" alt="" width="40px"></li>
+        <li><a href="#"></a><img src="imgs/instagram.png" alt="" width="40px"></li>
+        <li><a href="#"></a><img src="imgs/whatsapp.png" alt="" width="45px"></li>
+        <li><a href="#"></a><img src="imgs/tiktok.png" alt="" width="40px"></li>
       </ul>
     </div>
 
